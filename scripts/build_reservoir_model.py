0e6b77d491ace5e8df50bd0b68fc89084fdb96d8
djb-geochem <djbyrne20@gmail.com>
2025-11-17 21:37:24 -0800

    built grid_generator.py and implemented in build_reservoir_model.py with template

--- scripts/build_reservoir_model.py
+++ scripts/build_reservoir_model.py
@@ -5,16 +5,40 @@ Created on Thu Oct 30 20:53:46 2025
 
 @author: user
 """
+import sys
+import os
+import pathlib
+from jinja2 import Environment, FileSystemLoader
 
-def 
+#change sys.path to project root
+PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
+os.chdir(PROJECT_ROOT)
+
+from utils.grid_generator import generate_grid, format_grid
+from utils import load_yaml
 
 if __name__ == "__main__":
     
-    load_yaml()
+    res_params = load_yaml("reservoir_model_parameters")[0]
     
-    do_some_processing()
+    res_params["dx"] = generate_grid(res_params["nx"],
+                                     res_params["len_x"])
     
-    generate_fields()
+    res_params["formatted_grid"] = format_grid(res_params["dx"])
+    print(res_params["formatted_grid"])
     
-    write_template()
+    #load template
+    template_dir = PROJECT_ROOT / "templates"
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template("reservoir.master")
     
+    #write template
+    
+    rendered_template = template.render(res_params)
+
+    output_dir = PROJECT_ROOT / "reservoir"
+    output_file = output_dir / "reservoir.in"
+    
+    with open(output_file, "w") as f:
+        f.write(rendered_template)
--- utils/grid_generator.py
+++ utils/grid_generator.py
@@ -8,16 +8,23 @@ Created on Thu Nov  6 15:14:41 2025
 
 import numpy as np
 
-x = np.logspace(-1, np.log10(20e3), 101)
+def generate_grid(nx, len_x=20e3):
 
-dx = np.diff(x)
+    x = np.logspace(-1, np.log10(len_x), nx+1)
+    
+    dx = np.diff(x)
+    
+    return dx
 
-with open("grid.txt", 'w') as writefile:
+def format_grid(dx):
+    
+    output = ["    "]
     
     for n, i in enumerate(dx):
-        writefile.write(np.format_float_scientific(i, precision=8))
-        writefile.write(" ")
-        if (n+1)%5 == 0:
-            writefile.write("\\")
-            writefile.write("\n")
-        
+        output.append(np.format_float_scientific(i, precision=8))
+        output.append(" ")
+        if (n+1)%5 == 0 and (n+1) != len(dx):
+            output.append("\\\n    ")
+    
+    return ''.join(output)
+
