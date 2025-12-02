fixed injectivity decline units for plot

utils/inj_decline_plotter.py
@@ -11,12 +11,6 @@ import numpy as np
 
 
 
-def plot_inj_decline():
-    
-    fig, ax = plt.subplots()
-    
-    
-
 
 def extract_inlet_flux(vtk_file, tol=1e-6):
     grid = pv.read(vtk_file)
@@ -34,21 +28,57 @@ def extract_inlet_flux(vtk_file, tol=1e-6):
     
     velocities = grid.cell_data["Vlx"]
     
-    return sum(velocities[cell_ids])
+    inlet_velocities = velocities[cell_ids]
+    
+    avg_velocity = sum(inlet_velocities)/len(inlet_velocities)
+          
+    return avg_velocity
 
 def plot_inj_decline(expt, nfiles=438):
     
     fig, ax = plt.subplots()
     fluxes = []
     
+    area = 2*3.14*0.1*300
+        
     for i in range(1, nfiles):
         
         filename = f"{expt}-vel-{i:03}.vtk"
         
-        fluxes.append(extract_inlet_flux(filename))
-
-    ax.plot(range(len(fluxes)), fluxes, 'r-')
+        avg_vel = extract_inlet_flux(filename)
+        
+        flux = avg_vel * area #m^3/s
+        
+        flux_t_d = flux*60*60*24
+        
+        fluxes.append(flux_t_d)
+        
+    x = np.array(range(len(fluxes))) * 100/24
     
+    ax.plot(x, fluxes, 'r-')
+    
+    ax.set_ylabel(r"$Injectivity\,\,(t/d)$")
+    ax.set_xlabel(r"$Time\,\,(d)$")    
     fig.savefig("figures/inj_decline.png")
-    
 
+def test():
+    fig, ax = plt.subplots()
+    
+    vels = extract_inlet_flux("reservoir-vel-001.vtk")
+    
+    print(vels)
+    
+    avg_vel = sum(vels)/len(vels)
+    
+    inlet_area = 2 * 3.14 * 0.1 * 300
+    
+    vol_flux = avg_vel * inlet_area #m^3/s
+    
+    
+    print(len(vels))
+    print(vol_flux)
+    
+    ax.plot(range(len(vels)), vels)
+
+
+plot_inj_decline("reservoir")